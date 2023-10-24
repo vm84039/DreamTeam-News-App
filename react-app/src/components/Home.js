1bfcfd38dd12e66e8322bddee2987cf5f78a0033
Tristram Reed <tar123@txstate.edu>
2023-10-24 14:21:19 -0500

    update

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -14,22 +14,26 @@ const Home = () => {
           <a href="#">Technology</a>
           <a href="#">Sports</a>
           <a href="#">Entertainment</a>
+          <a style={{position:"absolute", right:"0", paddingRight:"10px"}} href="#">Login</a>
       </nav>
 
       <main>
           <article>
               <h2>Sample Article 1</h2>
               <p>Content of the article goes here.</p>
+              <a href="https://google.com" target="_blank">Read More</a>
           </article>
 
           <article>
               <h2>Sample Article 2</h2>
               <p>Content of the article goes here.</p>
+              <a href="https://baeldung.com" target="_blank">Read More</a>
           </article>
 
           <article>
               <h2>Sample Article 3</h2>
               <p>Content of the article goes here.</p>
+              <a href="https://yahoo.com" target="_blank">Read More</a>
           </article>
       </main>
 
