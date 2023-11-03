865a690759485d3962f159f1624148360d48440c
vm84039 <vm84039@yahoo.com>
2023-11-03 14:08:23 -0700

    Create build-microservices.sh

---
+++ build-microservices.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Building Spring Boot .jar files..."
+sleep 3
+
+AUTHDIR="Authenticator"
+SCHEDULERDIR="scheduler"
+SPRINGAPIDIR="spring-api"
+
+cd ./microservices/$AUTHDIR
+
+echo "Building Authenticator..."
+echo "---------------------"
+echo "command: mvn clean install -DskipTests"
+mvn clean install -DskipTests
+sleep 3
+
+cd ../$SCHEDULERDIR
+echo "Building scheduler..."
+echo "---------------------"
+echo "command: mvn clean install -DskipTests"
+mvn clean install -DskipTests
+sleep 3
+
+cd ../$SPRINGAPIDIR
+echo "Building spring-api..."
+echo "---------------------"
+echo "command: mvn clean install -DskipTests"
+mvn clean install -DskipTests
+sleep 3
+
+echo "Microservices built successfully!"
