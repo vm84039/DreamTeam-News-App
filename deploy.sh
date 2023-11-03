a3596b17d439368ce1a6e9832e3711e90e046e4b
vm84039 <vm84039@yahoo.com>
2023-11-03 14:04:54 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,13 +12,10 @@ npm run build
 
 # Start microservices
 echo "Starting microservices..."
-cd ..
-java -jar ./microservices/Authenticator/target/Authenticator.jar &
-java -jar ./microservices/scheduler/target/scheduler.jar &
-java -jar ./microservices/spring-api/target/spring-api.jar &
+java -jar ../microservices/Authenticator/target/Authenticator.jar &
+java -jar ../microservices/scheduler/target/scheduler.jar &
+java -jar ../microservices/spring-api/target/spring-api.jar &
 
-# Start front-end server (assuming you are using serve package)
-cd react-app/build
-npx serve -s
-
-echo "Application deployed successfully!"
+# Start Node.js server
+echo "Starting Node.js server..."
+node index.js &
