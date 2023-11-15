6fc584cffa0d6ed82db3617862cd9b55d9a7b6a0
Kaylynnlaster <Kayannalynnlaster@gmail.com>
2023-11-15 13:16:50 -0600

    header card

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -34,25 +34,29 @@ const Home = () => {
             Register
           </a>
         </div>
-        <img src="./crest.png" alt="crest" />
-        <h1 className="title">Dreamtastic News</h1>
-        <h3 className="title">The fever dream you can't wake up from!</h3>
+        <div className="headers">
+          <h1 className="title">Dreamtastic News</h1>
+          <h3 className="title">The fever dream you can't wake up from!</h3>
+          <img className="image" align="middle" src="crest.png" alt="crest" />
+        </div>
       </header>
       <NavBar />
 
       <main className="bigStory">
         <div className="top">
           <h2>Top News Story</h2>
+          <h1 className="headA"> Article Title</h1>
         </div>
-        <div className="picture">
-          <img src="./crest.png" alt="picture" />
-        </div>
-        <div className="content">
-          <h2 className="headA"> Article Title</h2>
-          <p className="story"> content blah blah</p>
-          <a className="storyLink" href="#">
-            Read all about it
-          </a>
+        <div className="mainStory_content">
+          <div className="picture">
+            <img src="crest.png" alt="picture" />
+          </div>
+          <div className="content">
+            <p className="story"> content blah blah</p>
+            <a className="storyLink" href="#">
+              Read all about it
+            </a>
+          </div>
         </div>
       </main>
 
